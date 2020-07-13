d5d92e33670643441158488d61f6b4c1e02ab4df
irvankadhafi <irvan.kadhafi.tif18@polban.ac.id>
2020-07-13 22:36:32 +0700

    update api

--- app/src/main/java/com/uas/restaurantsearch/models/Constant.java
+++ app/src/main/java/com/uas/restaurantsearch/models/Constant.java
@@ -2,13 +2,12 @@ package com.uas.restaurantsearch.models;
 
 public class Constant {
 
-    public static final String API_KEY = "b712469651b07553385c419160a7465e ";
+    public static final String API_KEY = "960107d8976634184845e3ab8caa0854 ";
 
     public static final String BASE_URL = "https://developers.zomato.com/api/v2.1/";
     public static final String GET_CATEGORIES = "categories";
     public static final String SEARCH_RESTAURANTS = "search";
     public static final String SEARCH_LOCATIONS = "locations";
-    public static final String REVIEWS = "reviews";
     public static final int ROW_COUNT = 10;
     public static final String TYPE_CITY = "city";
 
