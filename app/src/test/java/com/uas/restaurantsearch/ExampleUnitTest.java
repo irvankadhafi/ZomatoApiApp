1ea0079b19ab3afbc977bf5716243e955b7fb5e3
bileviosa <nurallisha.rahmanda.tif18@polban.ac.id>
2020-07-16 20:07:33 +0700

    1 Unit test

--- app/src/test/java/com/uas/restaurantsearch/ExampleUnitTest.java
+++ app/src/test/java/com/uas/restaurantsearch/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.uas.restaurantsearch;
 
+import com.uas.restaurantsearch.models.Location;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +14,29 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+@RunWith(JUnit4.class)
+@SmallTest
 public class ExampleUnitTest {
+
+    private Location mLocation;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mLocation = new Location();
+    }
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void setCity (){
+        String city = "Bandung";
+        mLocation.setCity(city);
+        assertEquals(city, "Bandung");
     }
 }
